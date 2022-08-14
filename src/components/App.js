bd645f53301d885617480614153f7bc041b83378
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-14 15:52:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,84 +1,82 @@
-import React, { Component, useState, useEffect, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/App.css";
 
+// class App extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             renderBall: false,
+//             posi : 0,
+//             ballPosition: { left: "0px" }
+//         };
+//         this.renderChoice = this.renderBallOrButton.bind(this)
+//         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+//     };
+
+//     buttonClickHandler() {
+
+//    }
+//     renderBallOrButton() {
+// 		if (this.state.renderBall) {
+// 		    return <div className="ball" style={this.state.ballPosition}></div>
+// 		} else {
+// 		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+// 		}
+//     }
+
+//     // bind ArrowRight keydown event
+//     componentDidMount() {
+
+//     }
+
+//     render() {
+//         return (
+//             <div className="playground">
+//                 {this.renderBallOrButton()}
+//             </div>
+//         )
+//     }
+// }
+
+// export default App;
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [ballPosition, setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
-  });
+  const [posi, setPosi] = useState(0);
+//   const [ballPosition, setBallPosi] = useState({ left: 0 });
 
-  const start = () => {
-    setRenderBall(!renderBall);
+  const buttonClickHandler = () => {
+    setRenderBall(true);
   };
 
-  const keyHandler = (event) => {
-    let ballPositionClone = { ...ballPosition };
-
-    if (event.keyCode === 37) {
-      ballPositionClone.left =
-        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) -
-        5 +
-        "px";
-    } else if (event.keyCode === 39) {
-      ballPositionClone.left =
-        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) +
-        5 +
-        "px";
-    } else if (event.keyCode === 38) {
-      ballPositionClone.top =
-        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) -
-        5 +
-        "px";
-    } else if (event.keyCode === 40) {
-      ballPositionClone.top =
-        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) +
-        5 +
-        "px";
+  const eventHandler = (event) => {
+    switch (event.keyCode) {
+      case 39:
+        setPosi((posi) => posi + 5);
+        break;
+      default:
+        break;
     }
-    // ballPositionClone.left = (Number(ballPosition.left.slice(0,ballPosition.left.indexOf("p")))+5)+"px";
-    setBallPosition(ballPositionClone);
-    // console.log(ballPosition);
   };
 
   useEffect(() => {
-    window.addEventListener("keydown", keyHandler);
-    return () => window.removeEventListener("keydown", keyHandler);
-  });
+    document.addEventListener("keydown", eventHandler);
+  }, []);
 
-  const reset = () => {
-    setRenderBall(!renderBall);
-    setBallPosition({ left: "0px", top: "0px" });
+  const renderBallOrButton = () => {
+    if (renderBall) {
+      return (
+        <div
+          className="ball"
+          style={{
+            left: posi + "px",
+            position: "absolute",
+          }}
+        ></div>
+      );
+    }
+    return <button onClick={buttonClickHandler}>Click For One Ball</button>;
   };
-  const renderChoice = () => {};
-
-  let compo;
-  if (renderBall) {
-    compo = (
-      <div
-        className="ball"
-        style={{ left: ballPosition.left, top: ballPosition.top }}
-      ></div>
-    );
-  } else {
-    compo = (
-      <button onClick={start} className="start">
-        Start
-      </button>
-    );
-  }
-
-  return (
-    <div className="playground">
-      {compo}
-      <button onClick={reset} className="reset">
-        Reset
-      </button>
-      {renderChoice()}
-    </div>
-  );
+  return <div className="playground">{renderBallOrButton()}</div>;
 };
 
 export default App;
